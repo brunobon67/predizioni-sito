b6e358b3e7525037c541e52f15a42ea4b731a110
brunobon67 <bonifacio.bruno92@gmail.com>
2025-12-19 21:24:41 +0100

    Fix /api/teams: include teams from upcoming matches

--- backend/app.py
+++ backend/app.py
@@ -103,7 +103,7 @@ def get_teams():
 
     session = SessionLocal()
     try:
-      q = session.query(Match)
+    q = session.query(Match)
 
         if competition:
             q = q.filter(Match.competition == competition)
